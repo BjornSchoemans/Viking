3eb329a1801bdc3b3f856ffd91721dd980529101
WilmsenProductions <timwilmsen@hotmail.com>
2015-06-03 21:07:48 +0200

    lol

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function MoveShip() {
+    
+    function leftArrowPressed() {
+        var ship = document.getElementById("vikingBoot");
+        alert("Holo");
+    }
+    
+    function moveObject(e) {
+        switch (e.keyCode) {
+            case 37:
+                leftArrowPressed();
+                break;
+            case 39:
+                
+                break;
+            case 38:
+                
+                break;
+            case 40:
+                
+                break;
+        }
+    }
+    
+}
+
+
